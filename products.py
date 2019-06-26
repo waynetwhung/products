0965526ca48f3f0861a61f12871c07f033288def
Wayne Hung <thunder.wayne@gmail.com>
2019-06-26 16:26:41 +0800

    add check file

--- products.py
+++ products.py
@@ -1,30 +1,37 @@
+import os # operating system
+
 products = []
-# 讀取檔案
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		#跳過欄位名稱
-		if '品項,價格' in line:
-			continue
-		name, price = line.strip().split(',')
+
+if os.path.isfile('products.csv'):
+	print('yeah! 找到檔案了!')
+	# 讀取檔案
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			#跳過欄位名稱
+			if '品項,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案...')
+	# 讓使用者輸入
+	while True:
+		name = input('請輸入商品名稱： ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格： ')
+		# p = []
+		# p.append(name)
+		# p.append(price)
+
+		# p = [name, price]
+
+		# products.append(p)
+
 		products.append([name, price])
-print(products)
-
-# 讓使用者輸入
-while True:
-	name = input('請輸入商品名稱： ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格： ')
-	# p = []
-	# p.append(name)
-	# p.append(price)
-
-	# p = [name, price]
-
-	# products.append(p)
-
-	products.append([name, price])
-print(products)
+	print(products)
 
 # 印出所有購買紀錄
 for p in products:
